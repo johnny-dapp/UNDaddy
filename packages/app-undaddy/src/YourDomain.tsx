b4171a1a3b044bbcb04b4d9463eda0040a5b62e8
Cherry Liang <cherry@centrality.ai>
2019-06-27 14:40:36 +1200

    Bid page

--- packages/app-undaddy/src/YourDomain.tsx
+++ packages/app-undaddy/src/YourDomain.tsx
@@ -2,22 +2,71 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
-import React from "react";
-import { Bubble, InputAddress } from "@polkadot/ui-app";
-import { AccountIndex, Balance, Nonce } from "@polkadot/ui-reactive";
+import React from 'react';
+import { Bubble, InputAddress } from '@polkadot/ui-app';
+import { AccountIndex, Balance, Nonce } from '@polkadot/ui-reactive';
+import { List, Typography, Button, Icon } from 'antd';
+import styled from 'styled-components';
+import AccountSelector from './AccountSelector';
+
+const data = ['www.test.ai', 'www.winning.ai', 'www.blockchain.ai'];
 
 type Props = {
-  onChange: (accountId?: string) => void
+  onChange: (accountId?: string) => void;
+  domains: Array<string> | null;
 };
 
 type State = {
-  accountId?: string
+  accountId?: string;
 };
+
+const RenderItems = styled.div`
+  display: flex;
+  padding: 1rem;
+  justify-content: space-around;
+`;
+
+const RenderItem = styled.div`
+  flex: 1;
+  font-size: 18px;
+`;
+
+const ButtonGroup = styled.div`
+  flex: 1;
+`;
 
 export default class Bid extends React.PureComponent<Props, State> {
   state: State = {};
 
-  render() {
-    return <div>Your Domain</div>;
+  render () {
+    return (
+      <div>
+        <AccountSelector onChange={() => console.log('this')} />
+        <List
+          size='large'
+          header={
+            <div>
+              <Icon type='"account-book' style={{ color: 'rgba(0,0,0,.25)' }} /> Your domian
+            </div>
+          }
+          dataSource={data}
+          renderItem={item => (
+            <RenderItems>
+              <RenderItem>
+                <List.Item>{item}</List.Item>
+              </RenderItem>
+              <ButtonGroup>
+                <Button onClick={() => console.log('bid-domain:')} type='primary' style={{ margin: '1rem' }}>
+                  Publish
+                </Button>
+                <Button onClick={() => console.log('bid-domain:')} type='primary' style={{ margin: '1rem' }}>
+                  UNPublish
+                </Button>
+              </ButtonGroup>
+            </RenderItems>
+          )}
+        />
+      </div>
+    );
   }
 }
